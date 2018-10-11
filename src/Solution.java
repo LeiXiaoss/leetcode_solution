daca0c46155c1bf1936112a0158e2e7fc4260067
leixiao <2399520027@qq.com>
2018-10-11 17:24:34 +0800

    leetcode 11/12

--- src/Solution.java
+++ src/Solution.java
@@ -1,7 +1,4 @@
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class Solution {
     //给定一个整数数组和一个目标值，找出数组中和为目标值的两个数。
@@ -522,4 +519,110 @@ public class Solution {
     }
 
     //还有dp解法
+
+    //给定 n 个非负整数 a1，a2，...，an，每个数代表坐标中的一个点 (i, ai) 。在坐标内画 n 条垂直线，
+    // 垂直线 i 的两个端点分别为 (i, ai) 和 (i, 0)。
+    // 找出其中的两条线，使得它们与 x 轴共同构成的容器可以容纳最多的水。
+    //
+    //说明：你不能倾斜容器，且 n 的值至少为 2。
+    //
+    //图中垂直线代表输入数组 [1,8,6,2,5,4,8,3,7]。在此情况下，容器能够容纳水
+    // （表示为蓝色部分）的最大值为 49。
+    //
+    //
+    //
+    //示例:
+    //
+    //输入: [1,8,6,2,5,4,8,3,7]
+    //输出: 49
+
+    //假设 am与an间的乘积最大，则m的左边不存在比am大的值，n右边不存在比an大的值
+    public int maxArea(int[] height) {
+        if(height.length == 2){
+            return height[0]>height[1]?height[1]:height[0];
+        }
+
+        int start = 0,end = height.length-1;
+        int maxValue = 0;
+
+        while (start<end){
+            maxValue = Math.max((end-start)*
+                    Math.min(height[start],height[end]),maxValue);
+
+            if(height[start]>height[end]){
+                end--;
+            }else {
+                start++;
+            }
+        }
+
+        return maxValue;
+    }
+
+    //罗马数字包含以下七种字符： I， V， X， L，C，D 和 M。
+    //
+    //字符          数值
+    //I             1
+    //V             5
+    //X             10
+    //L             50
+    //C             100
+    //D             500
+    //M             1000
+    //
+    //例如， 罗马数字 2 写做 II ，即为两个并列的 1。12 写做 XII ，即为 X + II 。
+    // 27 写做  XXVII, 即为 XX + V + II 。
+    //
+    //通常情况下，罗马数字中小的数字在大的数字的右边。但也存在特例，
+    // 例如 4 不写做 IIII，而是 IV。数字 1 在数字 5 的左边，
+    // 所表示的数等于大数 5 减小数 1 得到的数值 4 。
+    // 同样地，数字 9 表示为 IX。这个特殊的规则只适用于以下六种情况：
+    //
+    //    I 可以放在 V (5) 和 X (10) 的左边，来表示 4 和 9。
+    //    X 可以放在 L (50) 和 C (100) 的左边，来表示 40 和 90。
+    //    C 可以放在 D (500) 和 M (1000) 的左边，来表示 400 和 900。
+    //
+    //给定一个整数，将其转为罗马数字。输入确保在 1 到 3999 的范围内。
+    //
+    //示例 1:
+    //
+    //输入: 3
+    //输出: "III"
+    //
+    //示例 2:
+    //
+    //输入: 4
+    //输出: "IV"
+    //
+    //示例 3:
+    //
+    //输入: 9
+    //输出: "IX"
+    //
+    //示例 4:
+    //
+    //输入: 58
+    //输出: "LVIII"
+    //解释: C = 100, L = 50, XXX = 30, III = 3.
+    //
+    //示例 5:
+    //
+    //输入: 1994
+    //输出: "MCMXCIV"
+    //解释: M = 1000, CM = 900, XC = 90, IV = 4.
+
+    public String intToRoman(int num) {
+        String str = new String();
+
+        String[] romanNum = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+        int[] value = {1000,900,500,400,100,90,50,40,10,9,5,4,1};
+
+        for(int i=0;num>0;i++){
+            while (num>=value[i]){
+                num = num - value[i];
+                str = str + romanNum[i];
+            }
+        }
+        return str;
+    }
 }
