d690d11e9c93622a9debeb965aef1b7e0c6f0c21
leixiao <2399520027@qq.com>
2018-10-16 20:31:55 +0800

    leetcode 15/16

--- src/Solution.java
+++ src/Solution.java
@@ -812,5 +812,90 @@ public class Solution {
         return true;
     }
 
+    //给定一个包含 n 个整数的数组 nums，判断 nums 中是否存在三个元素 a，b，c ，
+    // 使得 a + b + c = 0 ？找出所有满足条件且不重复的三元组。
+    //
+    //注意：答案中不可以包含重复的三元组。
+    //
+    //例如, 给定数组 nums = [-1, 0, 1, 2, -1, -4]，
+    //
+    //满足要求的三元组集合为：
+    //[
+    //  [-1, 0, 1],
+    //  [-1, -1, 2]
+    //]
+
+    public List<List<Integer>> threeSum(int[] nums){
+        List<List<Integer>> sumList = new ArrayList<>();
+
+        if(nums == null || nums.length == 0) return sumList;
+
+        Arrays.sort(nums);
+
+        for (int i=0;i<nums.length-2;i++){
+            if(i!=0&&nums[i] == nums[i-1]){
+                continue;
+            }
+            int start = i+1;
+            int end = nums.length-1;
+
+            while (start<end){
+                if(nums[i]+nums[start]+nums[end]==0){
+                    ArrayList<Integer> temp = new ArrayList<>();
+                    temp.add(nums[i]);
+                    temp.add(nums[start]);
+                    temp.add(nums[end]);
+                    sumList.add(temp);
+                    start++;
+                    end--;
+                    while (start<end&&nums[start] == nums[start-1]){
+                        start++;
+                    }
+                    while (start<end&&nums[end] == nums[end+1]){
+                        end--;
+                    }
+                }else if(nums[i]+nums[start]+nums[end]<0){
+                    start++;
+                }else {
+                    end--;
+                }
+            }
+        }
+        return sumList;
+    }
+
+    //给定一个包括 n 个整数的数组 nums 和 一个目标值 target。
+    // 找出 nums 中的三个整数，使得它们的和与 target 最接近。
+    // 返回这三个数的和。假定每组输入只存在唯一答案。
+    //
+    //例如，给定数组 nums = [-1，2，1，-4], 和 target = 1.
+    //
+    //与 target 最接近的三个数的和为 2. (-1 + 2 + 1 = 2).
+
+    public int threeSumClosest(int[] nums, int target) {
+        if(nums == null || nums.length < 3) return -1;
+
+        Arrays.sort(nums);
+        int bestSum = nums[0]+nums[1]+nums[2];
+
+        for (int i=0;i<nums.length-2;i++){
+            int start = i+1;
+            int end = nums.length-1;
+            while (start<end){
+                int sum = nums[i]+nums[start]+nums[end];
+                if(Math.abs(bestSum-target) > Math.abs(sum-target)){
+                    bestSum = sum;
+                }
+                if(sum>target){
+                    end--;
+                }else if(sum<target){
+                    start++;
+                }else {
+                    return target;
+                }
+            }
+        }
+        return bestSum;
+    }
 
 }
