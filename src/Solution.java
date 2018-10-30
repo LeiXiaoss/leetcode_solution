07cbcf59f9c2df0c1cd82c5d70fafe6ea3257e52
leixiao <2399520027@qq.com>
2018-10-30 16:46:54 +0800

    leetcode 25/26/27

--- src/Solution.java
+++ src/Solution.java
@@ -1290,4 +1290,173 @@ public class Solution {
         }
         return preHead.next;
     }
+
+    //给出一个链表，每 k 个节点一组进行翻转，并返回翻转后的链表。
+    //
+    //k 是一个正整数，它的值小于或等于链表的长度。如果节点总数不是 k 的整数倍，那么将最后剩余节点保持原有顺序。
+    //
+    //示例 :
+    //
+    //给定这个链表：1->2->3->4->5
+    //
+    //当 k = 2 时，应当返回: 2->1->4->3->5
+    //
+    //当 k = 3 时，应当返回: 3->2->1->4->5
+    //
+    //说明 :
+    //
+    //    你的算法只能使用常数的额外空间。
+    //    你不能只是单纯的改变节点内部的值，而是需要实际的进行节点交换。
+
+    //好难。。。
+    //链表
+    public ListNode reverseKGroup(ListNode head,int k){
+        if(head ==  null || k == 1) return head;
+        ListNode node = new ListNode(0);
+        ListNode pre = node;
+        node.next = head;
+        for (int i=1;head!=null;i++){
+            if(i%k == 0){
+                pre = reverse(pre,head.next);
+                head = pre.next;
+            }else {
+                head = head.next;
+            }
+        }
+        return node.next;
+    }
+
+    private ListNode reverse(ListNode pre, ListNode next) {
+        ListNode head = pre.next;
+        ListNode move = head.next;
+        while (move!=next){
+            head.next = move.next;
+            move.next = pre.next;
+            pre.next = move;
+            move = head.next;
+        }
+        return head;
+    }
+
+    //给定一个排序数组，你需要在原地删除重复出现的元素，使得每个元素只出现一次，返回移除后数组的新长度。
+    //
+    //不要使用额外的数组空间，你必须在原地修改输入数组并在使用 O(1) 额外空间的条件下完成。
+    //
+    //示例 1:
+    //
+    //给定数组 nums = [1,1,2],
+    //
+    //函数应该返回新的长度 2, 并且原数组 nums 的前两个元素被修改为 1, 2。
+    //
+    //你不需要考虑数组中超出新长度后面的元素。
+    //
+    //示例 2:
+    //
+    //给定 nums = [0,0,1,1,1,2,2,3,3,4],
+    //
+    //函数应该返回新的长度 5, 并且原数组 nums 的前五个元素被修改为 0, 1, 2, 3, 4。
+    //
+    //你不需要考虑数组中超出新长度后面的元素。
+    //
+    //说明:
+    //
+    //为什么返回数值是整数，但输出的答案是数组呢?
+    //
+    //请注意，输入数组是以“引用”方式传递的，这意味着在函数里修改输入数组对于调用者是可见的。
+    //
+    //你可以想象内部操作如下:
+    //
+    //// nums 是以“引用”方式传递的。也就是说，不对实参做任何拷贝
+    //int len = removeDuplicates(nums);
+    //
+    //// 在函数里修改输入数组对于调用者是可见的。
+    //// 根据你的函数返回的长度, 它会打印出数组中该长度范围内的所有元素。
+    //for (int i = 0; i < len; i++) {
+    //    print(nums[i]);
+    //}
+
+    //快、慢指针
+    public int removeDuplicates(int[] nums){
+        if(nums.length == 0|| nums.length == 1) return nums.length;
+
+        int i=0;
+        for (int j=1;j<nums.length;j++){
+            if(nums[j]!=nums[i]){
+                i++;
+                nums[i] = nums[j];
+            }
+        }
+        return i+1;
+    }
+
+    //给定一个数组 nums 和一个值 val，你需要原地移除所有数值等于 val 的元素，返回移除后数组的新长度。
+    //
+    //不要使用额外的数组空间，你必须在原地修改输入数组并在使用 O(1) 额外空间的条件下完成。
+    //
+    //元素的顺序可以改变。你不需要考虑数组中超出新长度后面的元素。
+    //
+    //示例 1:
+    //
+    //给定 nums = [3,2,2,3], val = 3,
+    //
+    //函数应该返回新的长度 2, 并且 nums 中的前两个元素均为 2。
+    //
+    //你不需要考虑数组中超出新长度后面的元素。
+    //
+    //示例 2:
+    //
+    //给定 nums = [0,1,2,2,3,0,4,2], val = 2,
+    //
+    //函数应该返回新的长度 5, 并且 nums 中的前五个元素为 0, 1, 3, 0, 4。
+    //
+    //注意这五个元素可为任意顺序。
+    //
+    //你不需要考虑数组中超出新长度后面的元素。
+    //
+    //说明:
+    //
+    //为什么返回数值是整数，但输出的答案是数组呢?
+    //
+    //请注意，输入数组是以“引用”方式传递的，这意味着在函数里修改输入数组对于调用者是可见的。
+    //
+    //你可以想象内部操作如下:
+    //
+    //// nums 是以“引用”方式传递的。也就是说，不对实参作任何拷贝
+    //int len = removeElement(nums, val);
+    //
+    //// 在函数里修改输入数组对于调用者是可见的。
+    //// 根据你的函数返回的长度, 它会打印出数组中该长度范围内的所有元素。
+    //for (int i = 0; i < len; i++) {
+    //    print(nums[i]);
+    //}
+
+    //快、慢指针
+    public int removeElement(int[] nums,int val){
+        if(nums.length == 0) return 0;
+
+        int i=0;
+        for (int j=0;j<nums.length;j++){
+            if(nums[j] != val){
+                nums[i] = nums[j];
+                i++;
+            }
+        }
+        return i;
+    }
+
+    //交换头尾节点，适合需要删除的元素比较少的情况
+    public int removeElement1(int[] nums,int val){
+        if(nums.length == 0) return 0;
+
+        int i=0,n=nums.length;
+        while (i<n){
+            if(nums[i] == val){
+                nums[i] = nums[n-1];
+                n--;
+            }else {
+                i++;
+            }
+        }
+        return n;
+    }
 }
