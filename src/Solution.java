260b44d863d7273930c0295336e07a5e5230a76a
leixiao <2399520027@qq.com>
2018-11-01 16:05:13 +0800

    leetcode 31

--- src/Solution.java
+++ src/Solution.java
@@ -1614,7 +1614,50 @@ public class Solution {
         return res;
     }
 
+    //实现获取下一个排列的函数，算法需要将给定数字序列重新排列成字典序中下一个更大的排列。
+    //
+    //如果不存在下一个更大的排列，则将数字重新排列成最小的排列（即升序排列）。
+    //
+    //必须原地修改，只允许使用额外常数空间。
+    //
+    //以下是一些例子，输入位于左侧列，其相应输出位于右侧列。
+    //1,2,3 → 1,3,2
+    //3,2,1 → 1,2,3
+    //1,1,5 → 1,5,1
+
+    public void nextPermutation(int[] nums){
+        int i = nums.length-2;
+        while (i>=0&&nums[i]>=nums[i+1]){
+            i--;
+        }
+
+        if(i>=0){
+            int j = nums.length-1;
+            while (j>=0&&nums[j]<=nums[i]){
+                j--;
+            }
+            swap(nums,i,j);
+        }
+        reserve(nums,i+1);
+    }
+
+    private void reserve(int[] nums, int i) {
+        int start = i;
+        int end = nums.length-1;
+        while(start<end){
+            swap(nums,start,end);
+            start++;
+            end--;
+        }
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
     public static void main(String[] args){
-        System.out.println(new Solution().findSubstring("barfoothefoobarman",new String[]{"foo","bar"}));
+        new Solution().nextPermutation(new int[]{1,2,3});
     }
 }
