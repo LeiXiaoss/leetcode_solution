71d0f2e433454992dcbc67617bc57cf229b28194
leixiao <2399520027@qq.com>
2018-11-06 16:53:38 +0800

    调整文件结构，新增leetcode 32/33/34

--- src/com/leixiao/leetcode/structure/ListNode.java
+++ src/com/leixiao/leetcode/structure/ListNode.java
@@ -1,6 +1,8 @@
+package com.leixiao.leetcode.structure;
+
 public class ListNode {
-    int val;
-    ListNode next;
+    public int val;
+    public ListNode next;
 
     public ListNode(int val) {
         this.val = val;
