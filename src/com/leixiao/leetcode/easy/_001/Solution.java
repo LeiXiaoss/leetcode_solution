27f50b20b7f66fb7ae3cd400b32c316aadf2fc6f
leixiao <2399520027@qq.com>
2018-11-10 22:31:26 +0800

    update leetcode 40

--- src/com/leixiao/leetcode/easy/_001/Solution.java
+++ src/com/leixiao/leetcode/easy/_001/Solution.java
@@ -4,6 +4,8 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Solution {
+    //数组，哈希表
+
     //给定一个整数数组和一个目标值，找出数组中和为目标值的两个数。
     //
     //你可以假设每个输入只对应一种答案，且同样的元素不能被重复利用。
--- src/com/leixiao/leetcode/medium/_040/Solution.java
+++ src/com/leixiao/leetcode/medium/_040/Solution.java
@@ -1,40 +1,43 @@
-package com.leixiao.leetcode.medium._039;
+package com.leixiao.leetcode.medium._040;
 
+import com.sun.scenario.effect.impl.state.LinearConvolveRenderState;
+
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
 
 public class Solution {
-    //给定一个无重复元素的数组 candidates 和一个目标数 target ，
-    // 找出 candidates 中所有可以使数字和为 target 的组合。
+    //给定一个数组 candidates 和一个目标数 target ，找出 candidates 中所有可以使数字和为 target 的组合。
     //
-    //candidates 中的数字可以无限制重复被选取。
+    //candidates 中的每个数字在每个组合中只能使用一次。
     //
     //说明：
     //
-    //    所有数字（包括 target）都是正整数。
+    //    所有数字（包括目标数）都是正整数。
     //    解集不能包含重复的组合。
     //
     //示例 1:
     //
-    //输入: candidates = [2,3,6,7], target = 7,
+    //输入: candidates = [10,1,2,7,6,1,5], target = 8,
     //所求解集为:
     //[
-    //  [7],
-    //  [2,2,3]
+    //  [1, 7],
+    //  [1, 2, 5],
+    //  [2, 6],
+    //  [1, 1, 6]
     //]
     //
     //示例 2:
     //
-    //输入: candidates = [2,3,5], target = 8,
+    //输入: candidates = [2,5,2,1,2], target = 5,
     //所求解集为:
     //[
-    //  [2,2,2,2],
-    //  [2,3,3],
-    //  [3,5]
+    //  [1,2,2],
+    //  [5]
     //]
 
-    //回溯法
     public List<List<Integer>> combinationSum(int[] candidates,int target){
         List<List<Integer>> list = new ArrayList<>();
         Arrays.sort(candidates);
@@ -48,13 +51,15 @@ public class Solution {
         if(target < 0){
             return false;
         } else if(target == 0){
-            list.add(new ArrayList<>(tempList));
+            if(!list.contains(new ArrayList<>(tempList))){
+                list.add(new ArrayList<>(tempList));
+            }
             return false;
         }else {
             for (int i=start;i<candidates.length;i++){
                 boolean flag;
                 tempList.add(candidates[i]);
-                flag = backtrack(list,tempList,candidates,target-candidates[i],i);
+                flag = backtrack(list,tempList,candidates,target-candidates[i],i+1);
                 tempList.remove(tempList.size()-1);
                 if(!flag){
                     break;
@@ -62,5 +67,14 @@ public class Solution {
             }
         }
         return true;
+    }
+
+    public static void main(String[] args){
+        Solution solution = new Solution();
+        List<List<Integer>> lists = solution.combinationSum(new int[]{10,1,2,7,6,1,5},8);
+
+        for (List list : lists){
+            System.out.println(list);
+        }
     }
 }
