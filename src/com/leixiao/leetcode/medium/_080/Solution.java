cdbca5f736b56ebe0db6ffaf59d2daae992a1dd9
leixiao <2399520027@qq.com>
2019-01-03 20:20:58 +0800

    update LeetCode 026/080

--- src/com/leixiao/leetcode/medium/_080/Solution.java
+++ src/com/leixiao/leetcode/medium/_080/Solution.java
@@ -1,26 +1,24 @@
-package com.leixiao.leetcode.easy._026;
+package com.leixiao.leetcode.medium._080;
 
 public class Solution {
-    //给定一个排序数组，你需要在原地删除重复出现的元素，使得每个元素只出现一次，返回移除后数组的新长度。
+    //给定一个排序数组，你需要在原地删除重复出现的元素，使得每个元素最多出现两次，返回移除后数组的新长度。
     //
     //不要使用额外的数组空间，你必须在原地修改输入数组并在使用 O(1) 额外空间的条件下完成。
     //
     //示例 1:
     //
-    //给定数组 nums = [1,1,2],
+    //给定 nums = [1,1,1,2,2,3],
     //
-    //函数应该返回新的长度 2, 并且原数组 nums 的前两个元素被修改为 1, 2。
+    //函数应返回新长度 length = 5, 并且原数组的前五个元素被修改为 1, 1, 2, 2, 3 。
     //
     //你不需要考虑数组中超出新长度后面的元素。
-    //
     //示例 2:
     //
-    //给定 nums = [0,0,1,1,1,2,2,3,3,4],
+    //给定 nums = [0,0,1,1,1,1,2,3,3],
     //
-    //函数应该返回新的长度 5, 并且原数组 nums 的前五个元素被修改为 0, 1, 2, 3, 4。
+    //函数应返回新长度 length = 7, 并且原数组的前五个元素被修改为 0, 0, 1, 1, 2, 3, 3 。
     //
     //你不需要考虑数组中超出新长度后面的元素。
-    //
     //说明:
     //
     //为什么返回数值是整数，但输出的答案是数组呢?
@@ -38,24 +36,10 @@ public class Solution {
     //    print(nums[i]);
     //}
 
-    //快、慢指针
     public int removeDuplicates(int[] nums){
-        if(nums.length == 0|| nums.length == 1) return nums.length;
-
-        int i=0;
-        for (int j=1;j<nums.length;j++){
-            if(nums[j]!=nums[i]){
-                i++;
-                nums[i] = nums[j];
-            }
-        }
-        return i+1;
-    }
-
-    public int removeDuplicates1(int[] nums){
         int i=0;
         for (int n : nums){
-            if (i<1||n>nums[i-1]){
+            if(i<2||n>nums[i-2]){
                 nums[i++] = n;
             }
         }
