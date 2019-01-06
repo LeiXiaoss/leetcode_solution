22efbd7fd4865ff226a3d5016a58393aff7aa03c
leixiao <2399520027@qq.com>
2019-01-06 22:45:16 +0800

    update LeetCode 086

--- src/com/leixiao/leetcode/medium/_086/Solution.java
+++ src/com/leixiao/leetcode/medium/_086/Solution.java
@@ -47,9 +47,30 @@ public class Solution {
 
                 pre = pre.next;
             }else {
+                cur = cur.next;
                 continue;
             }
         }
         return dummy.next;
+    }
+
+    public static void main(String[] args){
+        Solution solution = new Solution();
+
+        ListNode node1 = new ListNode(1);
+        ListNode node2 = new ListNode(4);
+        ListNode node3 = new ListNode(3);
+        ListNode node4 = new ListNode(2);
+        ListNode node5 = new ListNode(5);
+        ListNode node6 = new ListNode(2);
+
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+        node4.next = node5;
+        node5.next = node6;
+
+        ListNode head = solution.partition(node1,3);
+        return;
     }
 }
