5952ccd8ffd0974748ddc4b100d67d99025a5eb9
leixiao <2399520027@qq.com>
2019-01-10 20:46:50 +0800

    add TreeNode

---
+++ src/com/leixiao/leetcode/structure/TreeNode.java
@@ -0,0 +1,10 @@
+package com.leixiao.leetcode.structure;
+
+public class TreeNode {
+    int val;
+    TreeNode left = null;
+    TreeNode right = null;
+    TreeNode(int val){
+        this.val = val;
+    }
+}
