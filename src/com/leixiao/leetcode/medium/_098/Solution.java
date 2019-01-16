8f0e7ceff118231e9993a5e7689b5329090ecc7e
leixiao <2399520027@qq.com>
2019-01-16 21:00:14 +0800

    update leetcode 098

--- src/com/leixiao/leetcode/medium/_098/Solution.java
+++ src/com/leixiao/leetcode/medium/_098/Solution.java
@@ -3,6 +3,10 @@ package com.leixiao.leetcode.medium._098;
 import com.leixiao.leetcode.structure.TreeNode;
 import sun.reflect.generics.tree.Tree;
 
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Stack;
+
 public class Solution {
     //给定一个二叉树，判断其是否是一个有效的二叉搜索树。
     //
@@ -32,6 +36,7 @@ public class Solution {
     //解释: 输入为: [5,1,4,null,null,3,6]。
     //     根节点的值为 5 ，但是其右子节点值为 4 。
 
+    //递归
     double last = -Double.MAX_VALUE;
     public boolean isValidBST(TreeNode root){
         if(root == null) return true;
@@ -45,37 +50,60 @@ public class Solution {
         return false;
     }
 
+    //中序遍历，判断是否升序排列，全遍历，执行效率太低
     public boolean isValidBST1(TreeNode root){
-        if(root == null||(root.left==null&&root.right==null)) return true;
+        List<Integer> list = new LinkedList<>();
 
-        return isValidBST1(root, Double.MAX_VALUE, Double.MIN_VALUE);
-    }
+        Stack<TreeNode> stack = new Stack<>();
 
-    public boolean isValidBST1(TreeNode root,double max,double min){
-        if(root.val<=min || root.val>=max) return false;
+        while (root!=null || !stack.isEmpty()){
+            while (root != null){
+                stack.push(root);
+                root = root.left;
+            }
 
-        if(root.left != null){
-            if(root.left.val >= root.val||
-                    !isValidBST1(root.left,Math.min(root.val,max),min)){
-                return false;
+            if(!stack.isEmpty()){
+                root = stack.pop();
+                list.add(root.val);
+                root = root.right;
             }
         }
 
-        if(root.right != null){
-            if(root.right.val <= root.val||
-                    !isValidBST1(root.right,max,Math.max(root.val,min))){
+        return isOrder(list);
+    }
+
+    private boolean isOrder(List<Integer> list){
+        for (int i=1;i<list.size();i++){
+            if(list.get(i)<=list.get(i-1)){
                 return false;
             }
         }
         return true;
     }
 
+    //这种递归更好理解
+    public boolean isValidBST2(TreeNode root){
+        if(root == null) return true;
+
+        return isValidBST2(root,Long.MAX_VALUE,Long.MIN_VALUE);
+    }
+
+    private boolean isValidBST2(TreeNode root,long max,long min){
+        if(root == null) return true;
+        else if(root.val>=max||root.val<=min) return false;
+        else {
+            return isValidBST2(root.left,Math.min(max,root.val),min)
+                    &&isValidBST2(root.right,max,Math.max(min,root.val));
+        }
+    }
+
+
     public static void main(String[] args){
-        TreeNode root = new TreeNode(10);
-        TreeNode node1 = new TreeNode(5);
-        TreeNode node2 = new TreeNode(15);
-        TreeNode node3 = new TreeNode(6);
-        TreeNode node4 = new TreeNode(20);
+        TreeNode root = new TreeNode(5);
+        TreeNode node1 = new TreeNode(1);
+        TreeNode node2 = new TreeNode(4);
+        TreeNode node3 = new TreeNode(3);
+        TreeNode node4 = new TreeNode(6);
 
         root.left = node1;
         root.right = node2;
@@ -84,6 +112,6 @@ public class Solution {
         node2.right = node4;
 
         Solution solution = new Solution();
-        System.out.println(solution.isValidBST(root));
+        System.out.println(solution.isValidBST2(root));
     }
 }
