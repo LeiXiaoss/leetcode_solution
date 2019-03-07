0b431d772bb8a070efa03fa82cf536cb04b6acc6
leixiao <2399520027@qq.com>
2019-03-07 10:44:25 +0800

    update struct Node

---
+++ src/com/leixiao/leetcode/structure/Node.java
@@ -0,0 +1,12 @@
+package com.leixiao.leetcode.structure;
+
+public class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+
+    public Node(int val){
+        this.val = val;
+    }
+}
